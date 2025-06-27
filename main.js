2c132b6da2e8c3b8b41d24565889c45fc205fa56
siwali <sambuk744@gmail.com>
2025-06-27 10:05:29 +0700

    4.9

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ createApp({
         const image = ref('./assets/images/socks_green.jpg')
         const productName = ref('CAMT')
         const productLink = ref('https://www.camt.cmu.ac.th')
+        const inStock = ref(false)
+        const inventory = ref(5)
+        const onSale = ref(true)
         return {
-            product,image,productLink,productName
+            product,image,productLink,productName,inStock,inventory,onSale
         }
     }
 }).mount('#app')
